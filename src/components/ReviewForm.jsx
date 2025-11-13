5e779ceb2cddf8d9331b1cc38b403de1b013c59e
mirkobechini <mirkobechini@gmail.com>
2025-11-13 15:42:31 +0100

    Add: Added form for reviewing a movie

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,27 +1,68 @@
-export default function ReviewForm() {
+import axios from "axios";
+import { useState } from "react"
+
+const API_URL = "http://localhost:3000"
+
+export default function ReviewForm({id}) {
+    
+
+
+    const initialForm = {
+        name:"",
+        text: "",
+        vote: 0
+    }
+
+    const [formData, setFormData] = useState(initialForm)
+
+
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log(formData);
+
+        axios.post(`${API_URL}/movies/${id}/reviews`, formData,{
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        })
+        .then( res => {
+            console.log(res);
+            setFormData(initialForm)
+            
+        })
+        .catch(err => {
+            console.log(err);
+            
+        })
+
+        
+
+        
+    }
+
     return (
         <>
 
             <div className="container mb-3 card p-3 bg-dark-subtle">
 
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="row mb-3 justify-content-around py-1">
-                        <label for="name" className="col-sm-1 col-form-label text-center">Name</label>
+                        <label htmlFor="name" className="col-sm-1 col-form-label text-center">Name</label>
                         <div className="col-sm-10">
-                            <input type="email" className="form-control" id="name" placeholder="Anonymous" />
+                            <input type="text" className="form-control" id="name" placeholder="Anonymous" value={formData.name} onChange={(e)=>setFormData({...formData, name:e.target.value})}/>
                         </div>
                     </div>
                     <div className="row mb-3 justify-content-around py-1">
-                        <label for="Review" className="col-sm-1 col-form-label text-center">Review</label>
+                        <label htmlFor="Review" className="col-sm-1 col-form-label text-center">Review</label>
                         <div className="col-sm-10">
-                            <textarea className="form-control" id="Review" rows={3} />
+                            <textarea className="form-control" id="Review" rows={3} value={formData.text} onChange={(e)=>setFormData({...formData, text:e.target.value})}/>
                         </div>
                     </div>
                     <div className="row mb-3 justify-content-around py-1">
-                        <label for="vote" className="col-sm-1 col-form-label text-center">Vote</label>
+                        <label htmlFor="vote" className="col-sm-1 col-form-label text-center">Vote</label>
                         <div className="col-sm-10">
-                            <select className="form-select" id="vote">
-                                <option value="1" selected>1 - Really Bad</option>
+                            <select className="form-select" id="vote" value={formData.vote} onChange={(e)=>setFormData({...formData, vote:e.target.value})}>
+                                <option value="1" >1 - Really Bad</option>
                                 <option value="2">2 - Bad</option>
                                 <option value="3">3 - Average</option>
                                 <option value="4">4 - Good</option>
@@ -30,7 +71,6 @@ export default function ReviewForm() {
                         </div>
                     </div>
                     <div className="text-center">
-
                         <button type="submit" className="btn btn-dark">Add review</button>
                     </div>
                 </form>
