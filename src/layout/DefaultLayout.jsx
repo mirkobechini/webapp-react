f5ac6510132aab63387ede891ccf398047a7588b
mirkobechini <mirkobechini@gmail.com>
2025-11-13 20:49:22 +0100

    Add: loader working and styled

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,13 +1,19 @@
 import { Outlet } from "react-router-dom";
 import AppHeader from "../components/AppHeader";
 import AppFooter from "../components/AppFooter";
+import { useContext } from "react";
+import GlobalContext from "../context/GlobalContext";
+import Loader from "../components/Loader";
 
 export default function DefaultLayout({menu}) {
+
+    const { loading } = useContext(GlobalContext)
 
     return (
         <>
             <AppHeader menu = {menu}/>
             <main>
+                {loading && <Loader/>}
                 <Outlet/>
             </main>
             <AppFooter menu = {menu}/>
